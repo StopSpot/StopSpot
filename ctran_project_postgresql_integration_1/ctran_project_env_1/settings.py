c69119134a076523b8ad4fb2df714ab443db6cb1
Brian Allen <brianallen@FVFWN8UXHV22.local>
2019-10-22 12:32:29 -0700

    change to setting.py

--- ctran_project_postgresql_integration_1/ctran_project_env_1/settings.py
+++ ctran_project_postgresql_integration_1/ctran_project_env_1/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SysMap',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'put password here',
         'HOST': 'localhost'
     }
 }
